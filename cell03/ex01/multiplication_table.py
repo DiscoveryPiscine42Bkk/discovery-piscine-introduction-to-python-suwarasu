09a2ce6b28ff887423e503d6f90e10c05837f528
ssurawat <68160054@kmitl.ac.th>
2025-11-05 12:19:40 +0000

    msg

--- cell03/ex01/multiplication_table.py
+++ cell03/ex01/multiplication_table.py
@@ -1,4 +1,9 @@
-number = int(input("Enter a number. :\n"))
-for i in range(10):
-    result = i * number
-    print(f"{number} x {i} = {result}")
+i = 0
+while i <= 10:
+    print(f"Table de {i}:", end=" ")
+    j = 0
+    while j <= 10:
+        print(i * j, end=" ")
+        j += 1
+    print()
+    i += 1
