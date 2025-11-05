71c96c820374046c4dbc029b4e5d4b46f54d02d3
ssurawat <68160054@kmitl.ac.th>
2025-11-05 03:58:48 +0000

    msg

--- cell01/ex00/name.py
+++ cell01/ex00/name.py
@@ -1,4 +1,4 @@
 first_name = "Suwara"
 last_name = "Surawatanapong"
 
-print(first, last)
+print(first_name, last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Suwara"
+last_name = "Surawatanapong"
+whole_name = (first_name, last_name)
+print(whole_name)
