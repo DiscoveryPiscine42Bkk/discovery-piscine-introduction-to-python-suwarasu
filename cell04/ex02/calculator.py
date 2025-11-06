66aa3f8f0ddd674c081c4490426316e796caee07
ssurawat <68160054@kmitl.ac.th>
2025-11-06 09:19:02 +0000

    msg

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,8 @@
+num1 = float(input("Enter your first number: "))
+num2 = float(input("Enter your second number: "))
+
+print("Thank you!")
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} - {num2} = {num1 - num2}")
+print(f"{num1} / {num2} = {num1 / num2}")
+print(f"{num1} * {num2} = {num1 * num2}")
